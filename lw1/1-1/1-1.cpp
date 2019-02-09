a5ecbc501f792355ad048fe7240f0c9e5359dfa5
Ilya Matveev <matveeffilia@gmail.com>
2019-02-09 23:57:35 +0300

    Выполнено задание 1-1

---
+++ lw1/1-1/1-1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+
+int main()
+{
+	std::vector<size_t> v = { 15, 3, 9, 13, 5, 18, 7, 1, 14, 6 };
+
+	boost::timer t;
+	t.restart();
+
+	std::sort(v.begin(), v.end());
+	do
+	{
+		//copy(v.begin(), v.end(), std::ostream_iterator<size_t>(std::cout, " "));
+		//std::cout << std::endl;
+	} while (std::next_permutation(v.begin(), v.end()));
+
+	double duration = t.elapsed();
+	std::cout << duration << std::endl;
+
+	return 0;
+}
---
+++ lw1/1-1/pch.h
@@ -0,0 +1,10 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+#include <algorithm>
+#include <boost/timer.hpp>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+#endif //PCH_H
