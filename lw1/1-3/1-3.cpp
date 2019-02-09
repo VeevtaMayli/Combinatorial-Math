58f03edb55fec7e48be55eaa43deb48d69c0de3a
Ilya Matveev <matveeffilia@gmail.com>
2019-02-10 01:01:19 +0300

    Выполнено задание 1-3

---
+++ lw1/1-3/1-3.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+
+using namespace std;
+
+vector<size_t> GetRandomPermutation(const int maxValueNumber)
+{
+	vector<size_t> permutation(maxValueNumber);
+	iota(permutation.begin(), permutation.end(), 1);
+	
+	default_random_engine randomGenerator;
+	uniform_int_distribution<size_t> range(0, maxValueNumber - 1);
+
+	for (int i = 0; i < permutation.size(); i++)
+	{
+		size_t randomValue = range(randomGenerator);
+		swap(permutation[i], permutation[randomValue]);
+	}
+
+	return permutation;
+}
+
+int main(const int argc, const char* argv[])
+{
+	const int maxPosition = 10;
+
+	vector<size_t> permutation = GetRandomPermutation(maxPosition);
+
+	copy(permutation.begin(), permutation.end(), std::ostream_iterator<size_t>(std::cout, " "));
+	cout << endl;
+
+	return 0;
+}
---
+++ lw1/1-3/pch.h
@@ -0,0 +1,11 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <random>
+#include <string>
+#include <vector>
+
+#endif //PCH_H
