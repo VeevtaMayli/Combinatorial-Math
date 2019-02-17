fbcffe80eca52bc8f72a7869ebe860e8250944d3
Ilya Matveev <matveeffilia@gmail.com>
2019-02-17 14:17:58 +0300

    Выполнено задание 1-5

---
+++ lw1/1-5/1-5.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+
+using namespace std;
+
+int main()
+{
+	const int N = 100;
+	const int k = 5;
+
+	if (k > N)
+	{
+		cout << "Program error" << endl;
+		return 1;
+	}
+
+	char m = 1;
+	vector<int> c(k + 1);
+	c[0] = -1;
+	iota(c.begin() + 1, c.end(), 1);
+
+	boost::timer timer;
+	timer.restart();
+
+	while (m != 0)
+	{
+		vector<int> p = c;
+		do
+		{
+			//copy(p.begin() + 1, p.end(), std::ostream_iterator<size_t>(cout, " "));
+			//cout << endl;
+		} while (next_permutation(p.begin() + 1, p.end()));
+		m = k;
+		while (c[m] == N - k + m)
+		{
+			m--;
+		}
+		c[m]++;
+		for (size_t i = m + 1; i <= k; i++)
+		{
+			c[i] = c[i - 1] + 1;
+		}
+	}
+
+	cout << timer.elapsed() << endl;
+
+	return 0;
+}
