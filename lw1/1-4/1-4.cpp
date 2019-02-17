e1c5a1a11a972c89379e52a7bfb18b715b18bd07
Ilya Matveev <matveeffilia@gmail.com>
2019-02-17 13:42:06 +0300

    Выполнено задание 1-4

---
+++ lw1/1-4/1-4.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+
+using namespace std;
+
+int main()
+{
+	const int N = 100;
+	const int k = 5;
+
+	if (k > N)
+	{
+		cout << "Program error" << endl;
+		return 1;
+	}
+
+	char m = 1;
+	vector<int> c(k + 1);
+	c[0] = -1;
+	iota(c.begin() + 1, c.end(), 1);
+
+	boost::timer timer;
+	timer.restart();
+
+	while (m != 0)
+	{
+		//copy(c.begin() + 1, c.end(), std::ostream_iterator<size_t>(cout, " "));
+		//cout << endl;
+		m = k;
+		while (c[m] == N - k + m)
+		{
+			m--;
+		}
+		c[m]++;
+		for (size_t i = m + 1; i <= k; i++)
+		{
+			c[i] = c[i - 1] + 1;
+		}
+	}
+
+	cout << timer.elapsed() << endl;
+
+	return 0;
+}
---
+++ lw1/1-4/pch.h
@@ -0,0 +1,11 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+#include <algorithm>
+#include <boost/timer.hpp>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <vector>
+
+#endif //PCH_H
